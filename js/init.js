195b8eeb1b801f85c961a1a4d8083c2d3c20f432
Henry Zhou <hzhou1995@gmail.com>
2017-06-06 16:52:50 -0400

    Added icons, changed some css, added Play Labs CTA

--- js/init.js
+++ js/init.js
@@ -10,9 +10,9 @@
 		reset: 'full',
 		breakpoints: {
 			global:		{ range: '*', href: '/css/style.css', containers: 1400, grid: { gutters: 50 } },
-			wide:		{ range: '-1680', href: '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
+			//wide:		{ range: '-1680', href: '/css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
 			normal:		{ range: '-1280', href: '/css/style-normal.css', containers: 960, viewport: { scalable: false } },
-			narrow:		{ range: '-980', href: '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+			narrow:		{ range: '-1280', href: '/css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
 			narrower:	{ range: '-840', href: '/css/style-narrower.css', grid: { collapse: 1 } },
 			mobile:		{ range: '-736', href: '/css/style-mobile.css', containers: '100%', grid: { gutters: 15, collapse: 2 } }
 		},
